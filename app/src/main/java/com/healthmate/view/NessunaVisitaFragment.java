7b86aba3fd4cdb407262cbbb699ae88dfe945f89
Diana <dianagagliardi2003@gmail.com>
2024-12-12 14:35:50 +0100

    Add schermata nessuna visita prenotata

---
+++ app/src/main/java/com/healthmate/view/NessunaVisitaFragment.java
@@ -0,0 +1,11 @@
+package com.healthmate.view;
+
+import androidx.fragment.app.Fragment;
+
+import com.healthmate.R;
+
+public class NessunaVisitaFragment extends Fragment {
+    public NessunaVisitaFragment() {
+        super(R.layout.fragment_nessuna_visita);
+    }
+}
