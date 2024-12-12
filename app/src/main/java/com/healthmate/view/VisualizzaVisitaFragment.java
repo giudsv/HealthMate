c313ee780152df798ef2a4bf6eab67bdc56cb1ef
Francesco <f.cataffo1@studenti.unisa.it>
2024-12-12 15:21:22 +0100

    Feat Prenotazione Visita

--- app/src/main/java/com/healthmate/view/VisualizzaVisitaFragment.java
+++ app/src/main/java/com/healthmate/view/VisualizzaVisitaFragment.java
@@ -8,7 +8,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavAction;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.healthmate.R;
 import com.healthmate.database.AppDatabase;
 import com.healthmate.database.bean.Visita;
@@ -32,6 +37,7 @@ public class VisualizzaVisitaFragment extends Fragment {
         TextView dateField = view.findViewById(R.id.date_field_visita);
         TextView timeField = view.findViewById(R.id.time_field_visita);
         TextView locationField = view.findViewById(R.id.location_field_visita);
+        FloatingActionButton fab = view.findViewById(R.id.fab);
 
         AppDatabase db = AppDatabase.getDatabase(requireContext());
 
@@ -46,6 +52,12 @@ public class VisualizzaVisitaFragment extends Fragment {
                 timeField.setText(dataVisita.toLocalTime().toString());
                 locationField.setText(visita.getLuogo());
             });
+        });
+
+        // FAB Configuration
+        fab.setOnClickListener((View v) -> {
+            NavHostFragment.findNavController(this)
+                    .navigate(R.id.navigation_prenotazione_visita);
         });
     }
 }
