b098a8ba75d9f8f3a2c17ae029f6a228e66d0957
Pino003 <giamundoa@gmail.com>
2024-12-14 12:27:54 +0100

    Update Account DAO

--- app/src/main/java/com/healthmate/AccountDAO.java
+++ app/src/main/java/com/healthmate/AccountDAO.java
@@ -1,25 +1,28 @@
 package com.healthmate;
 
 import androidx.room.Dao;
-import androidx.room.Insert;
 import androidx.room.Query;
 
 import com.healthmate.database.bean.Medico;
 import com.healthmate.database.bean.Paziente;
 
+import java.time.Instant;
+
 @Dao
 public interface AccountDAO {
-
-    @Insert
-    void insertPaziente(Paziente paziente);
-
-    @Insert
-    void insertMedico(Medico medico);
 
     @Query("SELECT * FROM Paziente WHERE username = :username AND password = :password")
     Paziente getPaziente(String username, String password);
 
     @Query("SELECT * FROM Medico WHERE username = :username AND password = :password")
     Medico getMedico(String username, String password);
+
+    @Query("INSERT INTO Medico (nome, cognome, CF, data_nascita, cellulare, email, username, password, studio, numero_albo) " +
+            "VALUES (:nome, :cognome, :cf, :dataNascita, :cellulare, :email, :username, :password, :studio, :numeroAlbo)")
+    Medico insertMedico(String nome, String cognome, String cf, Instant dataNascita, String cellulare, String email, String username, String password, String studio, String numeroAlbo);
+
+    @Query("INSERT INTO Paziente (nome, cognome, CF, data_nascita, cellulare, email, username, password, sesso, email_tutore) " +
+            "VALUES (:nome, :cognome, :cf, :dataNascita, :cellulare, :email, :username, :password, :sesso, :emailTutore)")
+    Paziente insertPaziente(String nome, String cognome, String cf, Instant dataNascita, String cellulare, String email, String username, String password, String sesso, String emailTutore);
 
 }
